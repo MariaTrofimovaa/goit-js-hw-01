e8ca936e1a834e510002cc1215a2d508e78d2852
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-02-23 17:13:54 +0200

    added tasks

--- js/task-5.js
+++ js/task-5.js
@@ -1,47 +1,62 @@
 let country;
 let price;
-let message = 'Отменено пользователем';
+let message = "Отменено пользователем";
 let isValid = true;
 
-let userInput = prompt('Введите название страны');
+let userInput = prompt("Введите название страны");
 if (userInput !== null) {
-    switch (userInput.toLowerCase()) {
-    case 'китай':
-        country = 'Китай';
-        price = 100;
-        break;
-    
-    case 'чили':
-        country = 'Чили';
-        price = 250;
-        break;
-    
-    case 'австралия':
-        country = 'Австралию';
-        price = 170;
-        break;
-    
-    case 'индия':
-        country = 'Индию';
-        price = 80;
-        break;
-    
-    case 'ямайка':
-        country = 'Ямайку';
-        price = 120;
-        break;
-    
-        default:
-        isValid = false;
-        message = 'В вашей стране доставка не доступна';
-    } 
+  switch (userInput.toLowerCase()) {
+    case "китай":
+      country = "Китай";
+      price = 100;
+      break;
+
+    case "чили":
+      country = "Чили";
+      price = 250;
+      break;
+
+    case "австралия":
+      country = "Австралию";
+      price = 170;
+      break;
+
+    case "индия":
+      country = "Индию";
+      price = 80;
+      break;
+
+    case "ямайка":
+      country = "Ямайку";
+      price = 120;
+      break;
+
+    default:
+      isValid = false;
+      message = "В вашей стране доставка не доступна";
+  }
 } else {
-    isValid = false;
-} 
+  isValid = false;
+}
 
 if (isValid) {
-    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
 }
 
 alert(message);
 
+/*
+Пользователь может оформить доставку товара к себе в страну, указав ее при посещении страницы в prompt. Учти, пользователь может ввести имя страны не только буквами нижнего регистра, а к примеру 'кИтАЙ'.
+
+Напиши скрипт который выводит сообщение о стоимости доставки в указанную страну. Обязательно используй switch. Формат сообщения: 'Доставка в [страна] будет стоить [цена] кредитов'.
+
+Но доставка есть не везде, если указанной страны нет в списке, то выводи в alert сообщение 'В вашей стране доставка не доступна'.
+
+Ниже приведен список стран и стоимость доставки.
+
+Китай - 100 кредитов
+Чили - 250 кредитов
+Австралия - 170 кредитов
+Индия - 80 кредитов
+Ямайка - 120 кредитов
+*/
